ad5073bc9efeddc7ab681a313c85768677d25a59
poosdavid <64095123+poosdavid@users.noreply.github.com>
2020-04-26 11:56:20 +0200

    update second part

--- script.js
+++ script.js
@@ -2,3 +2,12 @@ let name = 'David';
 
 console.log(name + "'s apples");
 console.log('Hello World!');
+
+// így néz ki alapból, ide kell behelyettesíteni a dolgokat $('selector').action()
+
+$('h1').text('We are having a break until 17:05');
+//This is a JavaScript comment
+
+/* This is a multiline
+Javascript comment
+*/
